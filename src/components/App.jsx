0877a3fbbf82e81bad9e9256ff6e52ced00cc241
rmlyaaa <126497199+rmlyaaa@users.noreply.github.com>
2023-12-06 15:28:47 +0100

    done

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,61 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import { GlobalStyled } from './GlobalStyles';
+import { Component } from 'react';
+import { Section } from './Section/Section';
+import { FeedbackOptions } from './Options/Options';
+import { Statistics } from './Stats/Stats';
+import { Notification } from './Notification/Notification';
+
+export class App extends Component {
+  state = {
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  };
+
+  onLeaveFeedback = options => {
+    this.setState(prevState => ({ [options]: prevState[options] + 1 }));
+  };
+
+  countTotalFeedback = () => {
+    const { good, neutral, bad } = this.state;
+    return good + neutral + bad;
+  };
+
+  countPositiveFeedbackPercentage = () => {
+    const { good, neutral, bad } = this.state;
+    if (neutral > 0 || bad > 0) {
+      return Math.round(100 / (this.countTotalFeedback() / good));
+    }
+    return 100;
+  };
+
+  render() {
+    const options = Object.keys(this.state);
+    const { good, neutral, bad } = this.state;
+    return (
+      <>
+        <GlobalStyled />
+        <Section title={'Please leave feedback'}>
+          <FeedbackOptions
+            options={options}
+            onLeaveFeedback={this.onLeaveFeedback}
+          />
+        </Section>
+
+        <Section title={'Statistics'}>
+          {this.countTotalFeedback() > 0 ? (
+            <Statistics
+              good={good}
+              neutral={neutral}
+              bad={bad}
+              total={this.countTotalFeedback()}
+              positivePercentage={this.countPositiveFeedbackPercentage()}
+            />
+          ) : (
+            <Notification message={'There is no feedback'} />
+          )}
+        </Section>
+      </>
+    );
+  }
+}
---
+++ src/components/Options/Options.styled.js
@@ -0,0 +1,16 @@
+import styled from 'styled-components';
+
+export const Button = styled.button`
+  background-color: #fff;
+  height: 28px;
+  font-size: 16px;
+  cursor: pointer;
+  text-transform: capitalize;
+  display: flex;
+`;
+
+export const List = styled.ul`
+  display: flex;
+  justify-content: center;
+  gap: 12px;
+`;
